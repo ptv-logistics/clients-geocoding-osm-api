3d00707ca66d5d3055cae1ea27ec8034506f3e14
MachineUserPTV <github@ptvgroup.com>
2023-04-25 13:52:16 +0000

    Automatic synchronisation from developer.myptv.com

---
+++ typescript/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BoundingBox';
+export * from './CausingError';
+export * from './DetailLevel';
+export * from './ErrorResponse';
+export * from './Place';
+export * from './PlacesSearchResult';
+export * from './ReferencePosition';
+export * from './Warning';
